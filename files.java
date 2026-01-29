44fd19e40f4011bb3f4124b690335f8baff57c10
26wilseya-creator <26wilseya@albanyacademy.org>
2026-01-29 13:52:26 +0000

    draft

--- files.java
+++ files.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class files{
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File ("animals.txt");
+        File file = new File ("numbers.txt");
         Scanner scanner = new Scanner (file);
         String line = "";
         
@@ -18,7 +18,27 @@ public class files{
         System.out.println(counter);
         scanner.close();
 
+
         Scanner scanner2 = new Scanner (file);
-        
+        //String[] names = new String[counter];
+        int count = 0;
+        int [] numList = new int [count];
+        count = 0;
+        while (scanner2.hasNextLine()) {
+            line = scanner2.nextLine().trim();
+            int num = 0;
+            if (!line.isEmpty()) {
+                
+                num = Integer.parseInt(line);
+                numList[count] = num;
+                count++;
+            }
+        }
+
+        for (int i = 0; i < numList.length; i++) {
+            System.out.println(numList[i]);
+        }
+        System.out.println(numList[1] + numList[3]);
+        scanner2.close();
     }
 }
